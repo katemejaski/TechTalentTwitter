c8904d40cbaae3ec8ab821229ebebb18b1e92118
katemejaski <katmackjj@gmail.com>
2021-06-01 16:59:48 -0400

    Initial commit

---
+++ src/main/java/com/tts/TechTalentTwitter/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.tts.TechTalentTwitter.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
